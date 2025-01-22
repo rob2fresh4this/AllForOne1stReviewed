814096f7e2d8e27e6739aeed63c08d884e881442
rob2fresh4this <grijrob07@gmail.com>
2025-01-22 09:07:38 -0800

    add initial configuration and settings files for AllForOne project

--- obj/Release/net8.0/AllForOne.AssemblyInfo.cs
+++ obj/Release/net8.0/AllForOne.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AllForOne")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18354db4d979b9b4fd5c036e7b9654886899508e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AllForOne")]
